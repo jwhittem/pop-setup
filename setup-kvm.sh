9b8215d63fae0c1db96dc6b15344fa0eaa9e102f
Jeremy Whittemore <44034529+jwhittem@users.noreply.github.com>
2024-10-17 15:07:24 -0400

    updating needed scripts.

--- setup-kvm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-sudo apt -y install bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm virt-manager
-sudo adduser jeremy kvm
-@echo reboot
--- setup-zfs.sh
+++ setup-zfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 sudo apt update
-sudo apt install zfsutils-linux zfs-dkms
+sudo apt install -y zfsutils-linux zfs-dkms
 sudo modprobe zfs
 sudo zpool import -f data-pool
